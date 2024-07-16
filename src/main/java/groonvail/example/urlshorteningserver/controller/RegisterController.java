f16c68d276f3c4200ed9fe45ebe46695733d7346
박동윤 (Park Dong-Yun) <ehddbs7458@gmail.com>
2024-07-16 22:23:14 +0900

    release v2: thymeleaf ui without input error handling

--- src/main/java/groonvail/example/urlshorteningserver/controller/RegisterController.java
+++ src/main/java/groonvail/example/urlshorteningserver/controller/RegisterController.java
@@ -8,8 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -24,9 +27,26 @@ public class RegisterController {
     @Value("${protocol.http}")
     private String httpProtocol;
 
-    @PostMapping("/register")
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping(path = "/register", consumes = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String register(@RequestBody String urlString, @RequestHeader(HttpHeaders.HOST) String host) throws URISyntaxException {
+        return shortenUrl(urlString, host);
+    }
+
+    @PostMapping(path = "/register", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String registerByForm(@RequestParam String url,
+                                 @RequestHeader(HttpHeaders.HOST) String host,
+                                 RedirectAttributes redirectAttributes) throws URISyntaxException {
+        redirectAttributes.addFlashAttribute("shortenedUrl", shortenUrl(url, host));
+        return "redirect:/register";
+    }
+
+    private String shortenUrl(String urlString, String host) throws URISyntaxException {
         URI url = new URI(urlString);
         String shortenedPath = shortenedPathGenerator.generate(url);
         shortenedPathRepository.save(urlString, shortenedPath);
